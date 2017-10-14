7dc8612a92b51b9b8f37f23ae5a703717b5c1545
CppCommons <cppcommons@gmail.com>
2017-10-14 18:27:37 +0900

    Modified   yyy/dub.json Missing    yyy/src/app.d Add        yyy/source/app.d

--- yyy/source/app.d
+++ yyy/source/app.d
@@ -1,9 +1,14 @@
 import std.stdio;
 
+int add2(int a, int b) { return a + b; }
+
 unittest {
+  x;
+  writeln("test start");
   assert(1==1);
-  assert(1==2);
+  assert(1==add2(1,2));
   assert(1==1);
+  writeln("test success");
 }
 
 version(unittest) {}
