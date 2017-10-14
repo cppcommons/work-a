6f36137ce798eb64cb9c6ca02925782fa12b5cc8
CppCommons <cppcommons@gmail.com>
2017-10-14 18:55:53 +0900

    Modified   yyy/sub.d

--- yyy/sub.d
+++ yyy/sub.d
@@ -1,15 +1,20 @@
 module sub;
 
-import std.stdio;
+//import std.stdio;
+import core.stdc.stdio;
+import core.stdc.stdlib;
+import core.stdc.string;
 
 int add2(int a, int b) { return a + b; }
 
 unittest {
-  writeln("test start");
+  fprintf(stderr, "[stderr]\n");
+  printf("[stdout]\n");
+  //writeln("test start");
   assert(1==1);
   assert(1==add2(1,2));
   assert(1==1);
-  writeln("test success");
+  //writeln("test success");
 }
 
 
