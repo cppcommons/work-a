a4319d83d36f495cda8e0cdcc2de7c80ff0e97e6
CppCommons <cppcommons@gmail.com>
2017-10-16 19:14:55 +0900

    Modified   dmdscript/src/app.d

--- dmdscript/src/app.d
+++ dmdscript/src/app.d
@@ -1,8 +1,12 @@
-import std.stdio;
+import core.stdc.stdio;
+import core.stdc.stdlib;
+import core.stdc.string;
+import core.thread;
 
-version(unittest) {}
-else
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+  fprintf(stdout, "test1\n");
+  printf("test2\n");
+  fflush(stdout);
+  Thread.sleep( dur!("msecs")( 5000 ) );
 }
