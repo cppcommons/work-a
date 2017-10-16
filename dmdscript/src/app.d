7026c233fa3d227ecad8c4d5effb01c063f39e7f
CppCommons <cppcommons@gmail.com>
2017-10-16 19:52:21 +0900

    Modified   dmdscript/src/app.d

--- dmdscript/src/app.d
+++ dmdscript/src/app.d
@@ -1,6 +1,7 @@
-import core.stdc.stdio;
-import core.stdc.stdlib;
-import core.stdc.string;
+import std.stdio;
+//import core.stdc.stdio;
+//import core.stdc.stdlib;
+//import core.stdc.string;
 import core.thread;
 
 import pegged.grammar;
@@ -9,15 +10,20 @@ import std.conv: to;
 
 void main()
 {
-  fprintf(stdout, "test1\n");
-  printf("%f\n", interpreter("1"));
+  //fprintf(stdout, "test1\n");
+  //printf("%f\n", interpreter("1"));
+  writeln(interpreter("1"));
+  auto p1 = Arithmetic("1+2");
+  writeln(p1.matches);
+  writeln(p1);
   assert(interpreter("-1") == -1.0);
   assert(interpreter("1+1") == 2.0);
   assert(interpreter("1-1") == 0.0);
-  printf("test2\n");
-  fflush(stdout);
+  //printf("test2\n");
+  stdout.flush(); // for std.stdio
+  //fflush(stdout); // for core.stdc.stdio
   //Thread.sleep( dur!("msecs")( 5000 ) );
-  getchar();
+  //getchar();
 }
 
 float interpreter(string expr)
@@ -28,6 +34,8 @@ float interpreter(string expr)
 
     float value(ParseTree p)
     {
+      //writeln(p.name);
+      //stdout.flush();
         switch (p.name)
         {
             case "Arithmetic":
@@ -63,3 +71,111 @@ float interpreter(string expr)
 
     return value(p);
 }
+
+/+
+struct ParseTree
+{
+    string name; /// The node name
+    bool successful; /// Indicates whether a parsing was successful or not
+    string[] matches; /// The matched input's parts. Some expressions match at more than one place, hence matches is an array.
+
+    string input; /// The input string that generated the parse tree. Stored here for the parse tree to be passed to other expressions, as input.
+    size_t begin, end; /// Indices for the matched part (from the very beginning of the first match to the last char of the last match.
+
+    ParseTree[] children; /// The sub-trees created by sub-rules parsing.
+
+    /**
+    Basic toString for easy pretty-printing.
+    */
+    string toString(string tabs = "") const
+    {
+        string result = name;
+
+        string childrenString;
+        bool allChildrenSuccessful = true;
+
+        foreach(i,child; children)
+        {
+            childrenString ~= tabs ~ " +-" ~ child.toString(tabs ~ ((i < children.length -1 ) ? " | " : "   "));
+            if (!child.successful)
+                allChildrenSuccessful = false;
+        }
+
+        if (successful)
+        {
+            result ~= " " ~ to!string([begin, end]) ~ to!string(matches) ~ "\n";
+        }
+        else // some failure info is needed
+        {
+            if (allChildrenSuccessful) // no one calculated the position yet
+            {
+                Position pos = position(this);
+                string left, right;
+
+                if (pos.index < 10)
+                    left = input[0 .. pos.index];
+                else
+                    left = input[pos.index-10 .. pos.index];
+                //left = strip(left);
+
+                if (pos.index + 10 < input.length)
+                    right = input[pos.index .. pos.index + 10];
+                else
+                    right = input[pos.index .. $];
+                //right = strip(right);
+
+                result ~= " failure at line " ~ to!string(pos.line) ~ ", col " ~ to!string(pos.col) ~ ", "
+                       ~ (left.length > 0 ? "after " ~ left.stringified ~ " " : "")
+                       ~ "expected "~ (matches.length > 0 ? matches[$-1].stringified : "NO MATCH")
+                       ~ ", but got " ~ right.stringified ~ "\n";
+            }
+            else
+            {
+                result ~= " (failure)\n";
+            }
+        }
+
+        return result ~ childrenString;
+    }
+
+    @property string failMsg()
+    {
+        foreach(i, child; children)
+        {
+            if (!child.successful)
+                return child.failMsg;
+        }
+
+        if (!successful)
+        {
+            Position pos = position(this);
+            string left, right;
+
+            if (pos.index < 10)
+                left = input[0 .. pos.index];
+            else
+                left = input[pos.index - 10 .. pos.index];
+
+            if (pos.index + 10 < input.length)
+                right = input[pos.index .. pos.index + 10];
+            else
+                right = input[pos.index .. $];
+
+            return "Failure at line " ~ to!string(pos.line) ~ ", col " ~ to!string(pos.col) ~ ", "
+                ~ (left.length > 0 ? "after " ~ left.stringified ~ " " : "")
+                ~ "expected " ~ (matches.length > 0 ? matches[$ - 1].stringified : "NO MATCH")
+                ~ `, but got ` ~ right.stringified;
+        }
+
+        return "Success";
+    }
+
+    ParseTree dup() @property
+    {
+        ParseTree result = this;
+        result.matches = result.matches.dup;
+        result.children = map!(p => p.dup)(result.children).array();
+        return result;
+    }
+}
++/
