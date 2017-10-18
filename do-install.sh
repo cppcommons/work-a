9c14e204fbdd9c4fc9f2e3018b6c529033e18ad6
CppCommons <cppcommons@gmail.com>
2017-10-18 17:34:23 +0900

    Added      cl0 Added      d0 Added      vc0 Modified   do-install.sh

--- do-install.sh
+++ do-install.sh
@@ -1,7 +1,10 @@
 #!bash -uvx
 mkdir -p /usr/local/bin
-install em /usr/local/bin
+#install em /usr/local/bin
 install sjis /usr/local/bin
+install vc0 /usr/local/bin
+install cl0 /usr/local/bin
+install d0 /usr/local/bin
 install vc32 /usr/local/bin
 install cl32 /usr/local/bin
 install d32 /usr/local/bin
