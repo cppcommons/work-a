583e3c99114e221d4d96b63aa44a770ea5a2bdcd
root@win10 <root@win10>
2017-10-19 18:00:30 +0900

    .

--- yyy/test.bat
+++ yyy/test.bat
@@ -4,7 +4,7 @@ setlocal
 call "C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/vcvarsall.bat" x86
 echo on
 ::C:\D\dmd2\windows\bin\dmd.exe -m32mscoff -I=lib app2.d lib/pegged-ms32.lib winhttp.lib
-lib /def:sqlite3.def /machine:x86 /out:sqlite3-ms32.lib
+if not exist sqlite3-ms32.lib lib /def:sqlite3.def /machine:x86 /out:sqlite3-ms32.lib
 endlocal
 if %errorlevel% neq 0 (
   echo errorlevel is %errorlevel%.
