ce213d70e410f2b42b05f761dbcfcfc643e82c70
Your Name <you@example.com>
2017-10-19 12:57:05 +0900

    .

--- yyy/sub.d
+++ yyy/sub.d
@@ -1,23 +1,26 @@
-module sub;
-
 //import std.stdio;
 import core.stdc.stdio;
 import core.stdc.stdlib;
 import core.stdc.string;
 
-int add2(int a, int b) { return a + b; }
+int add2(int a, int b)
+{
+  return a + b;
+}
 
-unittest {
+unittest
+{
   fprintf(stderr, "[stderr]\n");
   printf("[stdout]\n");
   //writeln("test start");
-  assert(1==1);
-  assert(3==add2(1,2));
-  assert(1==1);
+  assert(1 == 1);
+  assert(3 == add2(1, 2));
+  assert(1 == 1);
   //writeln("test success");
 }
 
-
-unittest {
-  assert(3==add2(1,2));
+@safe
+unittest
+{
+  assert(3 == add2(1, 2));
 }
